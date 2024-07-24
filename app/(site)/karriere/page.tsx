b110c3cd0f86ad0cf4fe108b0d5f86f5dc8ecc57
wired87 <derbenedikt.sterra@gmail.com>
2024-07-24 22:28:58 +0200

    setup 06

--- app/(site)/karriere/page.tsx
+++ app/(site)/karriere/page.tsx
@@ -1,23 +1,10 @@
-import Hero from "@/components/Hero";
 import React from "react";
-import SectionHeader from "@/components/Common/SectionHeader";
-import Contact from "@/components/Contact";
+
 
 export default function KariereMain() {
   return (
     <>
-      <Hero
-        smallHeading={""}
-        actionButtonSection={<></>}
-        mainHeading={"Wer wir sind"}
-        explanationOrComponent={"Unser Team vereint individuelle Stärken und langjährige Expertise in der erfolgreichen Umsetzung von Softwareprojekten."}
-        endingText={""}
-        noContact={true}
-        imageOrSection={<AboutImageSection />}
-      />
-      <SectionHeader headerInfo={} />
 
-      <Contact />
     </>
   );
 }
