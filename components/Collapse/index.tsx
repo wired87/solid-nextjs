46c6c470face7922bc7d9f2906ee09533d072751
wired87 <derbenedikt.sterra@gmail.com>
2024-07-26 16:17:51 +0200

    bug

--- components/Collapse/index.tsx
+++ components/Collapse/index.tsx
@@ -1,12 +1,14 @@
-import {AccordionItem} from "@nextui-org/accordion";
+//import {AccordionItem} from "@nextui-org/accordion";
 import IntegrationsContent from "@/components/Collapse/Content/integrationsTable";
 
 const SingleAccordeon = () => {
   return(
-    <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1" className={"w-full"}>
-      <IntegrationsContent />
-    </AccordionItem>
+    <></>
   )
 }
-
+/*
+<AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1" className={"w-full"}>
+      <IntegrationsContent />
+    </AccordionItem>
+ */
 export default SingleAccordeon;
