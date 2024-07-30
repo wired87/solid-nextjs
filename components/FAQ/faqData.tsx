8fad9462d7dd0a7fbdd22bebb74aace056384e98
wired87 <derbenedikt.sterra@gmail.com>
2024-07-30 19:06:24 +0200

    fetaures 02

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -6,23 +6,32 @@ export const botFaqData: FAQ[] = [
   {
     id: 1,
     quest: "Welche Vorteile bietet ein AI-Agent?",
-    ans: "Steigern Sie die Zufriedenheit Ihrer Kunden, indem Sie ihnen unmittelbare Informationen auf Ihrer Webseite bereitstellen – ohne lästige Suchprozesse. Durch die nahtlose Integration in Ihre bestehenden Systeme und Kanäle, wie beispielsweise CRM, Wissensdatenbanken oder Social Media, wird Ihr Chatbot zu einem zentralen Anlaufpunkt für alle Anliegen Ihrer Kunden. Profitieren Sie von unserer Expertise und unseren Partnertools, um eine maßgeschneiderte Lösung zu entwickeln, die Ihre Kunden begeistert und Ihr Unternehmen voranbringt."
+    ans: "Ein KI-Agent steigert die Kundenzufriedenheit durch sofortige Informationen und " +
+      "nahtlose Integration in Ihre Systeme. Profitieren Sie von unserer Expertise für eine " +
+      "maßgeschneiderte Lösung, die Ihr Unternehmen voranbringt."
   },{
     id: 321,
     quest: "Was genau beinhaltet Ihr Service?",
-    ans: "Wir entwickeln maßgeschneiderte KI-Chatbots, die sich nahtlos in Ihre bestehenden Systeme integrieren, damit Sie sich auf Ihr Kerngeschäft konzentrieren können. Unsere fundierte Expertise im Bereich Künstlicher Intelligenz und der Einsatz führender Technologien garantieren Ihnen einen Chatbot, der höchsten Qualitätsansprüchen genügt und Ihre Kunden durch exzellenten Service begeistert.",
+    ans: "Wir erstellen wir mit Ihnen einen Plan welcher optimal zu Ihrem Unternehmen passt." +
+      "",
   },{
     id: 99,
     quest: "Wie geht es nach beendigung des Auftrags weiter?",
-    ans: "Nach beendigung des Auftrags und vollständiger Integration werden Ihnen die Zugangsdaten zu unserer Partner übergeben. Alternativ bieten wir Ihnen die Möglichkeit uns nach beendigung des auftrags weiter um den bot zu kümmern um bestehende updates, ausfälle u.a. für Sie nicht zum Problem werden zu lassen.",
+    ans: "Nach erfolgreicher Integration übergeben wir Ihnen die Zugangsdaten " +
+      "unserer Partnertools. Auf Wunsch kümmern wir uns auch gerne weiterhin um " +
+      "Updates und Wartung Ihres Bots, damit Sie sich entspannt zurücklehnen können" +
+      "und Ihrem Kerngeschäft folgen können.",
   },{
     id: 15,
     quest: "Bleibt der Chatbot auch bei Änderungen meiner Webseite aktuell?",
-    ans: 'Bei aktualisierung der Knowledgbase wird diese grundlegend aktualisiert und neu aufgebaut um Ihre Kunden auf dem neusten stand zu halten'
+    ans: 'Bei aktualisierung der Knowledgebase wird diese grundlegend ' +
+      'aktualisiert und neu aufgebaut um Ihre Kunden auf dem neusten stand zu halten'
   },{
     id: 19,
     quest: "Ich bin interessiert! Wie geht es weiter?",
-    ans: 'Super, legen wir los, nachdem Sie unsere Kontaktform ausgefüllt haben welche sie über den Reiter <Get in touch!> erreichen, werden wir uns innerhalb der nächsten 2 Werktage mit Ihnen in verbindung setzen um die nächsten schritte zu bespechen.'
+    ans: 'Super, legen wir los, nachdem Sie unsere Kontaktform ausgefüllt haben, werden wir uns innerhalb' +
+      ' der nächsten 2 Werktage mit Ihnen in verbindung setzen um die nächsten schritte zu besprechen. ' +
+      '- Selbstverständlich komplett unverbindlich.'
   },
 ]
 
